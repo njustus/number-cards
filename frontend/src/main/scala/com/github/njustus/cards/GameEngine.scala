5af0ba9a0c79cd41a42645a4efcc3dd83a14ba26
N. Justus <n-justus@outlook.com>
2023-03-18 18:50:27 +0100

    update player's hand on DrawCard, CardPlayed

--- frontend/src/main/scala/com/github/njustus/cards/GameEngine.scala
+++ frontend/src/main/scala/com/github/njustus/cards/GameEngine.scala
@@ -1,31 +1,38 @@
 package com.github.njustus.cards
 
 import com.github.njustus.cards.GameTable.GameState
-import com.github.njustus.cards.shared.dtos.Player
+import com.github.njustus.cards.shared.dtos._
 import com.github.njustus.cards.shared.events._
 import org.scalajs.dom.console
 
 object GameEngine {
   def applyEvent(currentPlayer: Player, event: GameEventEnvelope)(state: GameState): GameState = event.payload match {
     case DrawCard if state.closedCards.nonEmpty =>
-      //TODO add to hand if event from currentPlayer
       val (topCard :: tail) = state.closedCards
 
       state.copy(
-        tail,
-        topCard :: state.playedCards,
-        state.cardsPerPlayer.updatedWith(currentPlayer.name) {
-          case Some(cards) =>
-            Some(topCard::cards)
-          case None => None
+        closedCards = tail,
+        cardsPerPlayer = updatePlayer(state, event.sender) { cards =>
+          topCard::cards
         }
       )
     case CardPlayed(card) =>
       //TODO check for incorrect moves
-      //TODO remove from hand
-      state.copy(playedCards = card :: state.playedCards)
+      state.copy(
+        playedCards = card :: state.playedCards,
+        cardsPerPlayer = updatePlayer(state, event.sender) { cards =>
+          cards.filter(card2 => card2 != card)
+        }
+      )
     case ev =>
       console.warn(s"Unknown event: $ev")
       state
   }
+
+  private def updatePlayer(state: GameState, playerName: String)(fn: List[Card] => List[Card]): Map[String, List[Card]] =
+    state.cardsPerPlayer.updatedWith(playerName) {
+      case Some(cards) =>
+        Some(fn(cards))
+      case None => None
+    }
 }
